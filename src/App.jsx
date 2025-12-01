1ebae74ece700edc3105e69d6aae7dfab63d6533
bogdan_dima <bogdan_dima@ukr.net>
2025-12-01 19:58:22 +0200

    categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,72 +2,107 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
-import Categories from "./pages/Categories";
+import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import ProductDetales from "./pages/ProductDetales";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "about",
-    element: (
-      <>
-        <Header />
-        <About />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "cart",
-    element: (
-      <>
-        <Header />
-        <Cart />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "categories",
-    element: (
-      <>
-        <Header />
-        <Categories />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "not_found",
-    element: (
-      <>
-        <Header />
-        <NotFound />
-      </>
-    ),
-  },
-  {
-    path: "product_detales",
-    element: (
-      <>
-        <Header />
-        <ProductDetales />
-      </>
-    ),
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "about",
+        element: <About />,
+        children: [{ path: "", element: <Home /> }],
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+        children: [{ path: "", element: <Home /> }],
+      },
+      {
+        path: "category",
+        element: <Category />,
+        children: [{ path: "", element: <Home /> }],
+      },
+      {
+        path: "not_found",
+        element: <NotFound />,
+        children: [{ path: "", element: <Home /> }],
+      },
+      {
+        path: "product_detales",
+        element: <ProductDetales />,
+        children: [{ path: "", element: <Home /> }],
+      },
+    ],
   },
 ]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Header />
+//         <Home />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "about",
+//     element: (
+//       <>
+//         <Header />
+//         <About />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "cart",
+//     element: (
+//       <>
+//         <Header />
+//         <Cart />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "categories",
+//     element: (
+//       <>
+//         <Header />
+//         <Categories />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "not_found",
+//     element: (
+//       <>
+//         <Header />
+//         <NotFound />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "product_detales",
+//     element: (
+//       <>
+//         <Header />
+//         <ProductDetales />
+//       </>
+//     ),
+//   },
+// ]);
 function App() {
   return <RouterProvider router={router} />;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import { Link, NavLink } from "react-router-dom";
+import { categories } from "../data/data";
+
 function Home() {
   return (
     <div>
-      <NavLink to={"/about"}>About</NavLink>
-      <NavLink to={"/cart"}>Cart</NavLink>
-      <NavLink to={"/"}>Home</NavLink>
+      <h1>Категорії</h1>
+      <ul style={{ display: "flex" }}>
+        {categories.map((item) => (
+          <li key={item.id}>
+            <Link to={`/category/${item.name}`}>
+              {item.name}
+              <img src={item.img} alt={item.name} style={{ width: "150px" }} />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
