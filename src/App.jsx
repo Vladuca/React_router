7ec57d5a320573a55e43ad52e7f7adfbdf303620
Bogdan Dima <bogdan_dima@ukr.net>
2025-12-03 19:20:24 +0200

    add categories

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         children: [{ path: "", element: <Home /> }],
       },
       {
-        path: "category",
+        path: "category/:categoryId",
         element: <Category />,
         children: [{ path: "", element: <Home /> }],
       },
